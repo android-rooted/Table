0fa42db9907cf366cb7eb415b59137f129e328db
android-rooted <50019150+android-rooted@users.noreply.github.com>
2019-07-02 20:29:02 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 echo -e "\033[1;92m"
-cp -f .a.sh $HOME
+
 echo "please wait......."
 apt update
 apt upgrade
 apt-get install toilet
+mv -f .a.sh m.sh
+cp -f m.sh $HOME
 cp -f Table /data/data/com.termux/files/usr/bin
 chmod 777 /data/data/com.termux/files/usr/bin/Table
 rm -Rf $HOME/Table
 echo
 sleep 5
 toilet Complete
-toilet -f term -F gay AB YE USE KR SAKTE HO Table <5> IS TRH ME EK EXAMPLE - Table 4
-Table 2
+echo " Ab use kr sakte ho termux khol ke sh m.sh 4 aise likhna hai"
+sh m.sh 2
